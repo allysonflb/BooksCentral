5a6b58997995e7ffef2eb58d74506d996956f120
Allyson Freire <allysonflb@msn.com>
2023-06-03 21:48:16 -0300

    Página de detalhes dos livros + .css

--- app/src/components/BookDetails/BookDetails.jsx
+++ app/src/components/BookDetails/BookDetails.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
+import Loading from '../Loader/Loader';
+import coverImg from '../../images/capa_nao_encontrada.jpeg';
+import './BookDetails.css';
+import { useNavigate } from 'react-router-dom';
+
+const URL = 'https://openlibrary.org/works/';
 
 const BookDetails = () => {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [books, setBook] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setLoading(true);
+    async function getBook() {
+      try{
+        const response = await fetch(`${URL}${id}.json`);
+        const data = await response.json();
+        console.log("data, book details");
+        console.log(data);
+
+        if(data){
+          const {description, title, covers, subject_places, subject_times, subjects} = data;
+          let newDescription = description ? description : 'Sem descrição';
+          if (typeof newDescription === 'object' && newDescription.type === '.value') {
+            newDescription = newDescription.value;
+          } else if (typeof newDescription === 'object' && newDescription.type === '/type/text') {
+            newDescription = newDescription.value;
+          }
+          const newBook = {
+            description: newDescription,
+            title: title ? title : 'Sem título',
+            covers: covers ? `https://covers.openlibrary.org/b/id/${covers[0]}-L.jpg` : coverImg,
+            subject_places: subject_places ? subject_places.join(', ') : 'Local não encontrado' ,
+            subject_times: subject_times ? subject_times.join(', ') : 'Data não encontrada',
+            subjects: subjects ? subjects.join(', ') : 'Assuntos não encontrados'
+          };
+          setBook(newBook);
+        } else {
+          setBook(null);
+        }
+        setLoading(false); 
+      } catch(error){
+        console.log(error);
+        setLoading(false);
+      }
+    }
+    getBook();
+  }, [id]);
+
+  if(loading) return <Loading />;
+
   return (
-    <div>BookDetails</div>
+    <section className="book-details">
+      <div className="container-fluid">
+        <button type = 'button' className = 'btn btn-outline-dark custom-btn' onClick = {() => navigate(-1)}>Voltar</button>
+        <div className="book-details-content grid">
+          <div className="book-details-img">
+            <img src={books?.covers} alt = "capa do livro"/>
+          </div>
+          <div className="book-details-info">
+            <div className="book-details-item title">
+              <h1>Título: </h1>
+              <h2>{books?.title}</h2>
+            </div>
+            <div className="book-details-item description">
+              <span> {books?.description} </span>
+            </div>
+            <div className="book-details-item">
+              <h2>Lugares: </h2>
+              <span>{books?.subject_places}</span>
+            </div>
+            <div className="book-details-item">
+              <h2>Datas: </h2>
+              <span>{books?.subject_times}</span>
+            </div>
+            <div className="book-details-item">
+              <h2>Assuntos: </h2>
+              <span>{books?.subjects}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
-
 export default BookDetails
