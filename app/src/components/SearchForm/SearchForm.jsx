a956be34e5f0bcce28751e1886a8882394b73ab0
Allyson Freire <allysonflb@msn.com>
2023-06-04 16:05:36 -0300

    Refatoração do searchform e do about

--- app/src/components/SearchForm/SearchForm.jsx
+++ app/src/components/SearchForm/SearchForm.jsx
@@ -1,53 +1,46 @@
-import React, {useRef, useEffect} from 'react'
+import React, { useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useGlobalContext } from '../../content';
-import "./SearchForm.css";
+import { Container, Form, Button } from 'react-bootstrap';
+import './SearchForm.css';
 
 const SearchForm = () => {
-  const {setSearchTerm, setResultTitle} = useGlobalContext();
+  const { setSearchTerm, setResultTitle } = useGlobalContext();
   const searchText = useRef('');
   const navigate = useNavigate();
 
   useEffect(() => searchText.current.focus(), []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     let tempSearch = searchText.current.value.trim();
-    if ((tempSearch.replace(/[^\w\s]/gi,"")).length === 0){
-      setSearchTerm("The Lord of Rings");
-      setResultTitle("Busque um livro...");
+    if ((tempSearch.replace(/[^\w\s]/gi, '')).length === 0) {
+      setSearchTerm('The Lord of Rings');
+      setResultTitle('Busque um livro...');
     } else {
       setSearchTerm(searchText.current.value);
     }
-      navigate('/books');
-    };
+    navigate('/books');
+  };
 
   return (
-    <div className="search-form">
-      <div className="container-fluid">
-        <div className="search-form">
-          <form
-            className="d-flex flex-column flex-md-row"
-            onSubmit={handleSubmit}
-          >
-            <input
-              className="form-control custom-input search-bar"
-              type="search"
-              placeholder="Pesquise seu livro (em inglês)"
-              ref={searchText}
-              aria-label="Search"
-            />
-            <button
-              className="btn btn-outline-dark ms-md-2"
-              type="submit"
-              onClick={handleSubmit}
-            >
-              Pesquisar
-            </button>
-          </form>
-        </div>
-      </div>
+    <div className='search-form'>
+      <Container fluid>
+        <Form className='d-flex flex-column flex-md-row' onSubmit={handleSubmit}>
+          <Form.Control
+            className='custom-input search-bar'
+            type='search'
+            placeholder='Pesquise seu livro (em inglês)'
+            ref={searchText}
+            aria-label='Search'
+          />
+          <Button className='btn-outline-dark ms-md-2' type='submit' onClick={handleSubmit}>
+            Pesquisar
+          </Button>
+        </Form>
+      </Container>
     </div>
   );
 };
 
-export default SearchForm
+export default SearchForm;
--- app/src/pages/About/About.jsx
+++ app/src/pages/About/About.jsx
@@ -1,35 +1,36 @@
 import React from "react";
 import "./About.css";
 import aboutImg from "../../images/Sobre.jpeg";
+import { Container, Row, Col } from "react-bootstrap";
 
 const About = () => {
   return (
     <section className="about">
-      <div className="container-fluid">
+      <Container fluid>
         <div className="section-title">
           <h2>Sobre</h2>
         </div>
-        <div className="about-content grid">
-          <div className="about-img">
-            <img src = {aboutImg} alt="about" />
-          </div>
-          <div className="about-text">
+        <Row className="about-content">
+          <Col md={6} className="about-img">
+            <img src={aboutImg} alt="about" />
+          </Col>
+          <Col md={6} className="about-text">
             <h2 className="about-title fs-26 ls-1">BooksCentral</h2>
-            <h9 className="fs-20">
+            <h6>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias, et
               soluta! Accusantium autem dolores repudiandae sed porro cupiditate
               cumque eum libero, impedit exercitationem iure repellat adipisci
               magnam necessitatibus, ducimus vero.
-            </h9>
-            <h9 className="fs-20">
+            </h6>
+            <h6>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
               Reiciendis, rem odio. Nulla sapiente, iste minus, quod eaque
               asperiores non blanditiis similique ipsam excepturi a optio
               exercitationem et inventore impedit voluptas.
-            </h9>
-          </div>  
-        </div>
-      </div>
+            </h6>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 };
