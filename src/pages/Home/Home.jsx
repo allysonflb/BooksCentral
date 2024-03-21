1b641af6de484b643f39623e702b26c90e6eb964
Allyson Freire <allysonflb@msn.com>
2024-03-21 20:25:36 -0300

    Tentativa de add web analytics vercel 1.0

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header';
 import { Outlet } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 const Home = () => {
   return (
     <main>
       <Header />
       <Outlet />
+      <Analytics />
     </main>
   )
 }
