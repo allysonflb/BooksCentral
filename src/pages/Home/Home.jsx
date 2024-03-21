5ae8e4a8417d2fdae7ab64d7dab9c953c8ea9e23
Allyson Freire <allysonflb@msn.com>
2024-03-21 20:48:38 -0300

    Fix import

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../../components/Header/Header';
 import { Outlet } from 'react-router-dom';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next'; 
+import { SpeedInsights } from '@vercel/speed-insights/react'; 
 
 const Home = () => {
   return (
